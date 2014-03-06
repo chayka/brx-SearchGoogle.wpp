ade9ecdec50f80821489b596893f99015b87a197
Boris Mossounov <borix@tut.by>
2014-03-06 17:44:31 +0300

    + google search

--- application/helpers/OptionHelper_brx_SearchGoogle.php
+++ application/helpers/OptionHelper_brx_SearchGoogle.php
@@ -3,10 +3,20 @@
 class OptionHelper_brx_SearchGoogle {
     public static function getOption($option, $default='', $reload = false){
         $key = 'brx_SearchGoogle.'.$option;
-        return get_site_option($key, $default, !$reload);
+        return get_option($key, $default, !$reload);
     }
     
     public static function setOption($option, $value){
+        $key = 'brx_SearchGoogle.'.$option;
+        return update_option($key, $value);
+    }
+    
+    public static function getSiteOption($option, $default='', $reload = false){
+        $key = 'brx_SearchGoogle.'.$option;
+        return get_site_option($key, $default, !$reload);
+    }
+    
+    public static function setSiteOption($option, $value){
         $key = 'brx_SearchGoogle.'.$option;
         return update_site_option($key, $value);
     }
